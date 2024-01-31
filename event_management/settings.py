0576b75715f3e44ee25ec887efcb6ad069fe70f6
alaminfaraji <alaminice1617@gmail.com>
2024-01-31 08:13:20 +0600

    issus cross origin

--- event_management/settings.py
+++ event_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'recent_events',
     'rest_framework',
 ]
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'event_management.urls'
